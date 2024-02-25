cebbe7f7ce3eadee5713cc2273ab555d5f436ba2
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-26 01:14:17 +0200

    expand schema

--- server.js
+++ server.js
@@ -4,15 +4,64 @@ const { graphqlHTTP } = require('express-graphql');
 
 const schema = buildSchema(`
     type Query  {
-        description: String
-        price: Float
+      products: [Product]
+      orders: [Order]
+    }
+
+    type Product {
+      id: ID!
+      description: String!
+      reviews: [Review]
+      price: Float! 
+    }
+
+    type Review {
+        rating: Int!
+        comment: String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product
+        quantity: Int!
     }
 `);
 
 
 const root = {
-    description: () => 'black box',
-    price: () => 12.12,
+    products: [
+    {
+        id: 'leatherbelt',
+        description: 'Leather Belt',
+        price: 25.00,
+    },
+    {
+        id: 'cottonsocks',
+        description: 'Cotton Socks',
+        price: 5.00,
+    },
+    ],
+    orders: [
+    {
+        date: '2021-01-01',
+        subtotal: 30.00,
+        items: [
+        {
+            product: {
+                id: 'leatherbelt',
+                description: 'Vintage Leather Belt',
+                price: 34.15,
+            },
+            quantity: 1,
+            },  
+            ]
+        }
+    ]
 };
  
 

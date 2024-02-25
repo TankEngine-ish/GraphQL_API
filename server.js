0b51ea04c835bd2b0fbd7b8b985762a3347818fa
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-25 23:31:17 +0200

    add first endpoint

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
+
+const schema = buildSchema(`
+    type Query  {
+        description: String
+        price: Float
+    }
+`);
+
+
+const root = {
+    description: () => 'black box',
+    price: () => 12.12,
+};
+ 
+
+const app = express();
+
+
+app.use('/graphql', graphqlHTTP ({
+        schema: schema,
+        rootValue: root,
+}));
+
+
+app.listen(3000, () => {
+    console.log('GraphQL server is running on port 3000');
+});

c04e393bc08ed597cecae91f617929311859db03
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-26 21:24:15 +0200

    create models

---
+++ orders/orders.model.js
@@ -0,0 +1,16 @@
+module.exports =  [
+        {
+            date: '2021-01-01',
+            subtotal: 30.00,
+            items: [
+            {
+                product: {
+                    id: 'leatherbelt',
+                    description: 'Vintage Leather Belt',
+                    price: 34.15,
+                },
+                quantity: 1,
+                },  
+                ]
+            }
+        ];
---
+++ products/products.model.js
@@ -0,0 +1,12 @@
+module.exports = [
+        {
+            id: 'leatherbelt',
+            description: 'Leather Belt',
+            price: 25.00,
+        },
+        {
+            id: 'cottonsocks',
+            description: 'Cotton Socks',
+            price: 5.00,
+        },
+        ];

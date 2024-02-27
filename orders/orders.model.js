3db3bd2791788d82f2b3e1e42282281b58517dd6
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2024-02-27 21:35:41 +0200

    finish orders query

--- orders/orders.model.js
+++ orders/orders.model.js
@@ -1,4 +1,4 @@
-module.exports =  [
+const orders =  [
         {
             date: '2021-01-01',
             subtotal: 30.00,
@@ -14,3 +14,12 @@ module.exports =  [
                 ]
             }
         ];
+
+function getAllOrders() {
+    return orders;
+}
+
+
+module.exports = {
+    getAllOrders,
+}
